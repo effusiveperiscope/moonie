96169cafcbc46932428d9d8ec7a550c0a4b3f383
Anonymous <>
2024-11-25 15:16:52 -0800

    Perform a parsing pass on construction of prompt editing controller to update unmatched tags early

--- lib/widgets/prompt_edit.dart
+++ lib/widgets/prompt_edit.dart
@@ -24,6 +24,7 @@ class PromptEditingController extends TextEditingController {
     this.reservedTagStyle,
   }) {
     reservedTags = List.from(se.reservedTags);
+    _parseXML(text);
   }
 
   @override
